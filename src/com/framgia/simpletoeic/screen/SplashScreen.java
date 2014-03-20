40ccb65c99808aaf13d6e275d056ff0123c84312
Dinhpc <pham.cao.dinh@framgia.com>
2014-03-20 13:57:49 +0700

    Compare

--- src/com/framgia/simpletoeic/screen/SplashScreen.java
+++ src/com/framgia/simpletoeic/screen/SplashScreen.java
@@ -12,7 +12,6 @@ public class SplashScreen extends BaseSimpleToeicActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 	}
 	
 	
