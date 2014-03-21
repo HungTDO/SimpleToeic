8f1697739fe0791a28f07868c8627edbdba64d19
hungtdo <yuchi.it@gmail.com>
2014-03-21 13:08:36 +0700

    Add Sliding menu List and add common method

--- src/com/framgia/simpletoeic/BaseSimpleToeicActivity.java
+++ src/com/framgia/simpletoeic/BaseSimpleToeicActivity.java
@@ -1,9 +1,14 @@
 package com.framgia.simpletoeic;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * @author HUNGTDO Base Simple Toeic class. Extended to another activity
@@ -11,8 +16,6 @@ import android.os.Bundle;
  * */
 public class BaseSimpleToeicActivity extends Activity {
 
-	private int modifier;
-	
 	protected BaseSimpleToeicActivity self;
 
 	protected SimpleToeicAppplication app;
@@ -49,4 +52,136 @@ public class BaseSimpleToeicActivity extends Activity {
 		intent.putExtras(bundle);
 		startActivity(intent);
 	}
+	
+	// ======================= KEYBOARD MANAGER =======================
+	/**
+	 * Show soft keyboard
+	 * 
+	 * @param editText
+	 */
+	protected void showKeyboard(EditText editText) {
+		InputMethodManager inputMethodManager = (InputMethodManager) this
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		if (inputMethodManager != null) {
+			inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+					0);
+		}
+	}
+
+	/**
+	 * Hide soft keyboard
+	 * 
+	 * @param editText
+	 */
+	protected void hideKeyboard(EditText editText) {
+		((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+				.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+	}
+	
+	// ======================= DIALOG MANAGER =======================
+
+
+		/**
+		 * Show confirm dialog
+		 * 
+		 * @param title
+		 * @param messageId
+		 * @param positiveLabelId
+		 * @param negativeLabelId
+		 * @param positiveOnClick
+		 * @param negativeOnClick
+		 */
+		public void showDialog(int titleId, int messageId, int positiveLabelId,
+				int negativeLabelId,
+				DialogInterface.OnClickListener positiveOnClick,
+				DialogInterface.OnClickListener negativeOnClick) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getString(titleId));
+			builder.setMessage(getString(messageId));
+			builder.setPositiveButton(getString(positiveLabelId), positiveOnClick);
+			builder.setNegativeButton(getString(negativeLabelId), negativeOnClick);
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
+
+		/**
+		 * Get dialog confirm
+		 * 
+		 * @param title
+		 * @param messageId
+		 * @param positiveLabelId
+		 * @param negativeLabelId
+		 * @param positiveOnClick
+		 * @param negativeOnClick
+		 */
+		public void showDialog(String title, String message, String positiveLabel,
+				String negativeLabel,
+				DialogInterface.OnClickListener positiveOnClick,
+				DialogInterface.OnClickListener negativeOnClick, boolean isCancelled) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(title);
+			builder.setMessage(message);
+			builder.setPositiveButton(positiveLabel, positiveOnClick);
+			builder.setNegativeButton(negativeLabel, negativeOnClick);
+			builder.setCancelable(isCancelled);
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
+		
+		/**
+		 * Show information dialog
+		 * 
+		 * @param messageId
+		 * @param positiveLabelId
+		 * @param positiveOnClick
+		 */
+		public void showDialog(int messageId, int positiveLabelId,
+				DialogInterface.OnClickListener positiveOnClick, boolean isCancelled) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(""/* getString(R.string.app_name) */);
+			builder.setMessage(getString(messageId));
+			builder.setPositiveButton(getString(positiveLabelId), positiveOnClick);
+			builder.setCancelable(isCancelled);
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
+
+		// ======================= TOAST MANAGER =======================
+
+		/**
+		 *  Show long toast message
+		 * @param str alert message
+		 */
+		public void showToastMessage(String str) {
+			Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+		}
+
+		/**
+		 *  Show short toast message
+		 * @param str alert message
+		 */
+		public void showShortToastMessage(String str) {
+			Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+		}
+
+		/**
+		 *  Show toast message
+		 * @param str alert message
+		 * @param time long or short time
+		 *           
+		 */
+		public void showToastMessage(String str, int time) {
+			Toast.makeText(this, str, time).show();
+		}
+
+		/**
+		 *  Show toast message
+		 * @param resId resource string
+		 * @param time long or short time
+		 *           
+		 */
+		public void showToastMessage(int resId, int time) {
+			Toast.makeText(this, resId, time).show();
+		}
+
 }
